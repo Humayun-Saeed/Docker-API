f4763e88e8ef080d738cb34958409dd7f78104c5
Humayun-Saeed <humayunsaeed267@gmail.com>
2022-05-17 12:05:52 +0500

    main file

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express=require('express')
 var app=express()
 
+app.get('/admin',(req,res)=>{
+res.json("wellcome to the admin side: ")
+})
+app.get('/host',(req,res)=>{
+res.json("wellcome to the host side : ")
+})
+app.get('/',(req,res)=>{
+    console.log("wellcome to the home  side: ");
+})
 
-
+console.log("hello wot docker")
 app.listen(3000,()=>{
     console.log("connected to the server: ");
 })
